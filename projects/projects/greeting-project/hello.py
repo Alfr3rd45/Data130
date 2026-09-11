1b2048c073140a332f9734d4e28481510c339da3
Alfr3rd45 <alfredmoyo777@gmail.com>
2026-09-11 05:52:05 -0700

    Enhance greeting with data science topic inquiry
    Added a prompt for user interest in data science.

--- projects/projects/greeting-project/hello.py
+++ projects/projects/greeting-project/hello.py
@@ -1,6 +1,8 @@
 # My first Python project in GitHub
 def greet_user():
 name = input("Enter your name: ")
+topic = input("what area of data science intrests you the most?")
 print(f"Hello, {name}! Welcome to Data Science.")
+print(f"Awesome today we will be looking at, {topic}! hope you enjoy this."
 if __name__ == "__main__":
 greet_user()
